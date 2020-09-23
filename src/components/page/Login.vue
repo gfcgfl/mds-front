d4ecd568fd6359fa113826c1589ad6fa199bcf3b
gfcgfl <gfcgfl@163.com>
2020-09-23 21:31:10 +0800

    ooooo

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -60,28 +60,28 @@ export default {
     methods: {
         submitForm() {
 			// console.log(this);
-            request.post('/mds/login', {
-                userName: this.param.userName,
-                password: this.param.password
-            })
-                .then(res => {
-                    console.log(res)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-            // this.$refs.login.validate(valid => {
-            //     // console.log(valid);
-            //     if (valid) {
-            //         this.$message.success('登录成功');
-            //         localStorage.setItem('ms_username', this.param.username);
-            //         this.$router.push('/');
-            //     } else {
-            //         this.$message.error('请输入账号和密码');
-            //         console.log('error submit!!');
-            //         return false;
-            //     }
-            // });
+            // request.post('/mds/login', {
+            //     userName: this.param.userName,
+            //     password: this.param.password
+            // })
+            //     .then(res => {
+            //         console.log(res)
+            //     })
+            //     .catch(error => {
+            //         console.log(error)
+            //     })
+            this.$refs.login.validate(valid => {
+                // console.log(valid);
+                if (valid) {
+                    this.$message.success('登录成功');
+                    localStorage.setItem('ms_username', this.param.username);
+                    this.$router.push('/');
+                } else {
+                    this.$message.error('请输入账号和密码');
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
         },
     },
 };
